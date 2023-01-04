2aa5b48867536fe303c43f822aefc401ce111216
Lucas Marques <lucasg3.gdh@gmail.com>
2023-01-03 23:20:20 -0300

    First commit

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "1.3.6"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-00000001"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy = "Terraform"
+  }
+}
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.my-test-bucket.id
+  acl    = "private"
+}
